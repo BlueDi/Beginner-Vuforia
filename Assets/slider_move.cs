9125aa8f51e6c1c8189d04bde9768be606ce6a98
Diogo Cruz <diogohmcruz@hotmail.com>
2018-10-19 13:06:32 +0100

    Aula

---
+++ Assets/slider_move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class slider_move : MonoBehaviour {
+    public Slider sli;
+
+    // Use this for initialization
+    void Start () {
+        Slider sl = sli.GetComponent<Slider>();
+        sl.onValueChanged.AddListener(RotateMyObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RotateMyObject(float value)
+    {
+        float sliderValue = value;
+        transform.rotation = Quaternion.Euler(0, 0, sliderValue * 360 + 90);
+    }
+
+}

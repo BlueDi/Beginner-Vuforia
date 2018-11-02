15d7753e7463507290c65d7766c69a2bf34bdf73
Diogo Cruz <diogohmcruz@hotmail.com>
2018-11-02 17:27:01 +0000

    Button for Pause/Start
    The button is to be replaced by the Target interactions.

---
+++ Assets/Scripts/start_video.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class start_video : MonoBehaviour {
+    public Button btn;
+    public VideoPlayer videoPlayer;
+
+	// Use this for initialization
+	void Start () {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        btn.onClick.AddListener(PlayPause);
+    }
+	
+	// Update is called once per frame
+	void Update () {}
+
+    public void PlayPause(){
+        if (videoPlayer.isPlaying) {
+            videoPlayer.Pause();
+        } else {
+            videoPlayer.Play();
+        }
+    }
+}

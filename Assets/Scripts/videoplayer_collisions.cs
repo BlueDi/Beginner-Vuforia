7cbddf697bad65e605dcf0330f56d0fd5412d456
Diogo Cruz <diogohmcruz@hotmail.com>
2018-11-05 22:13:35 +0000

    Materials for VideoPlayer Menu

---
+++ Assets/Scripts/videoplayer_collisions.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class videoplayer_collisions : MonoBehaviour {
+    public VideoPlayer videoPlayer;
+    void onTriggerEnter(Collider other)
+    {
+        Debug.Log(other, this);
+    }
+    void onTriggerStay(Collider other)
+    {
+
+    }
+
+    void onTriggerExit(Collider other)
+    {
+
+    }
+
+    public void PlayPause()
+    {
+        if (videoPlayer.isPlaying)
+        {
+            videoPlayer.Pause();
+        }
+        else
+        {
+            videoPlayer.Play();
+        }
+    }
+
+    public void Forward5s()
+    {
+        videoPlayer.time += 5;
+    }
+
+    public void Backward5s()
+    {
+        videoPlayer.time -= 5;
+    }
+    public void IncreaseVolume()
+    {
+        float av = videoPlayer.GetDirectAudioVolume(0);
+        videoPlayer.SetDirectAudioVolume(0, av + 0.1F);
+    }
+
+    public void DecreaseVolume()
+    {
+        float av = videoPlayer.GetDirectAudioVolume(0);
+        videoPlayer.SetDirectAudioVolume(0, av - 0.1F);
+    }
+}

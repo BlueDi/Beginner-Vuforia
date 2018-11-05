dd0e62320b954202f32a73529cd0de3b1842af52
uno <nuno_mgl@hotmail.com>
2018-11-05 16:19:30 +0000

    added bolt and glow effect

---
+++ Assets/ShootButtonClick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootButtonClick : MonoBehaviour {
+
+    public GameObject sagira;
+    public Button btn;
+
+    const float COOLDOWN = 0.5F;
+
+    float cooldownTime = 0.0F;
+
+    void Start()
+    {
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void Update()
+    {
+        cooldownTime -= Time.deltaTime;
+    }
+
+    void OnClick()
+    {
+        if(cooldownTime <= 0.0F)
+        {
+            //
+            // TODO shoot
+            //
+
+            Transform sagiraTransform = sagira.GetComponent<Transform>();
+
+
+            cooldownTime = COOLDOWN;
+        }
+    }
+}

cb0eabc00c5079c93b34879076b0d74d512c6ec9
uno <nuno_mgl@hotmail.com>
2018-11-05 02:58:08 +0000

    meteo done

--- Assets/MeteoUpdate.cs
+++ Assets/MeteoUpdate.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Xml;
 using TMPro;
+using System;
+using System.Text;
 
 public class MeteoUpdate : MonoBehaviour {
 
@@ -14,6 +16,17 @@ public class MeteoUpdate : MonoBehaviour {
     public Text gpsConsole;
     public TextMeshPro frontMesh;
     public TextMeshPro backMesh;
+
+    public GameObject sunnyObj;
+    public GameObject sunnyAndCloudObj;
+    public GameObject cloudyObj;
+    public GameObject drizzleObj;
+    public GameObject rainObj;
+    public GameObject snowObj;
+    public GameObject fogObj;
+    public GameObject thunderObj;
+    public GameObject moonbObj;
+    public GameObject moonAndCloudObj;
 
     private bool updating = false;
     private float cooldown = 0.0F;
@@ -93,6 +106,17 @@ public class MeteoUpdate : MonoBehaviour {
         frontMesh.SetText("...");
         backMesh.SetText("...");
 
+        sunnyObj.SetActive(false);
+        sunnyAndCloudObj.SetActive(false);
+        cloudyObj.SetActive(false);
+        drizzleObj.SetActive(false);
+        rainObj.SetActive(false);
+        snowObj.SetActive(false);
+        fogObj.SetActive(false);
+        thunderObj.SetActive(false);
+        moonAndCloudObj.SetActive(false);
+        moonbObj.SetActive(false);
+
 
         if (latitude == 10000 || longitude == 10000)
         {
@@ -120,6 +144,11 @@ public class MeteoUpdate : MonoBehaviour {
 
                 XmlNode weatherIDXml = doc.SelectSingleNode("/current/weather/@number");
                 XmlNode weatherTempXml = doc.SelectSingleNode("/current/temperature/@value");
+                XmlNode timeRiseXml = doc.SelectSingleNode("/current/city/sun/@rise");
+                XmlNode timeSetXml = doc.SelectSingleNode("/current/city/sun/@set");
+
+                DateTimeOffset sunRise = DateTimeOffset.Parse(timeRiseXml.InnerText);
+                DateTimeOffset sunSet = DateTimeOffset.Parse(timeSetXml.InnerText);
 
 
                 string weatherId = "null";
@@ -127,6 +156,30 @@ public class MeteoUpdate : MonoBehaviour {
                 {
                     weatherId = weatherIDXml.InnerText;
                     collisionConsole.text = "Weather value: " + weatherId;
+
+                    int weatherIdnum = Int32.Parse(weatherId);
+
+
+                    if (weatherIdnum >= 200 && weatherIdnum <= 232)
+                        thunderObj.SetActive(true);
+                    else if (weatherIdnum >= 300 && weatherIdnum <= 321)
+                        drizzleObj.SetActive(true);
+                    else if (weatherIdnum >= 500 && weatherIdnum <= 531)
+                        rainObj.SetActive(true);
+                    else if (weatherIdnum >= 600 && weatherIdnum <= 622)
+                        snowObj.SetActive(true);
+                    else if (weatherIdnum >= 700 && weatherIdnum <= 781)
+                        fogObj.SetActive(true);
+                    else if (weatherIdnum == 800 && DateTime.Now >= sunRise && DateTime.Now < sunSet)
+                        sunnyObj.SetActive(true);
+                    else if (weatherIdnum == 800 && (DateTime.Now < sunRise || DateTime.Now >= sunSet))
+                        moonbObj.SetActive(true);
+                    else if (weatherIdnum >= 801 && weatherIdnum <= 802 && DateTime.Now >= sunRise && DateTime.Now < sunSet)
+                        sunnyAndCloudObj.SetActive(true);
+                    else if (weatherIdnum >= 801 && weatherIdnum <= 802 && (DateTime.Now < sunRise || DateTime.Now >= sunSet))
+                        moonAndCloudObj.SetActive(true);
+                    else if (weatherIdnum == 803 || weatherIdnum == 804)
+                        cloudyObj.SetActive(true);
                 }
 
                 string weatherTemp = "null";

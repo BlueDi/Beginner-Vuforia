dc213dfd650c565f469f8888a3bb01c4517bac68
uno <nuno_mgl@hotmail.com>
2018-11-07 00:38:24 +0000

    final

---
+++ Assets/SunnyAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunnyAnim : MonoBehaviour {
+
+    const int degreesPerSecond = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float degress = (float) degreesPerSecond * Time.deltaTime;
+
+        gameObject.transform.rotation *= Quaternion.Euler(0, 0, degress);
+    }
+}
---
+++ Assets/ThunderStruck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderStruck : MonoBehaviour {
+
+    public Light ligth1;
+    public Light ligth2;
+
+    float cooldown = 0.0F;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (ligth1.intensity == 100)
+            ligth1.intensity = 1;
+
+        if (ligth2.intensity == 100)
+            ligth2.intensity = 1;
+
+        if(cooldown <= 0)
+        {
+            ligth1.intensity = 100;
+            ligth2.intensity = 100;
+            cooldown = 3.0F;
+        }
+    }
+}
